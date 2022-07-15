00eca8bc6c50090dbb02da0329607736127efa94
Suraj Rai <surajrai@Surajs-MacBook-Pro.local>
2022-07-15 05:35:49 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+
+
+function App() {
+return (
+  <h1>Pokemon Relm</h1>
+)
+}
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
